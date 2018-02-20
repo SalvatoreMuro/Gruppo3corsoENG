70f3e33deb63747880695f67e023b08fd034b6e5
Isa <Isa@192.168.178.22>
2018-02-20 21:46:36 +0100

    aggiunge le modifiche fatte a laboratorio della giornata (classi test e user,...)

---
+++ projectwork-model/src/main/java/it/eng/projectwork/gruppo03/model/User.java
@@ -0,0 +1,50 @@
+package it.eng.projectwork.gruppo03.model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class User {
+
+	@Id
+	private String username;
+	private String nome;
+	private String cognome;
+	private String email;
+	
+	public User() {}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
