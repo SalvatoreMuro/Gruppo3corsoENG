ac0149605cf132d19d255fab0741fc9c96a56758
Rackox <Rackox@DESKTOP-53UT7PC>
2018-02-21 16:38:33 +0100

    Aggiunge i test per il Supplier

--- projectwork-model/src/test/java/it/eng/unipa/projectwork/model/test/JpacrudTest.java
+++ projectwork-model/src/test/java/it/eng/unipa/projectwork/model/test/JpacrudTest.java
@@ -6,7 +6,7 @@ import javax.persistence.Query;
 
 import org.junit.Test;
 
-import it.eng.projectwork.gruppo03.model.User;
+import it.eng.projectwork.gruppo03.model.*;
 
 public class JpacrudTest extends JPAHibernateTest {
 
@@ -41,5 +41,41 @@ public class JpacrudTest extends JPAHibernateTest {
 		 
 	}
 	
+	@Test
+	public void test3() {
+		em.getTransaction().begin();
+		
+		Supplier s = new Supplier();
+		s.setNome("giancarlo");
+		s.setCognome("compagno");
+		s.setUsername("giacompa");
+		s.setEmail("giancarlo.compagno@eng.it");
+		s.setNomeAzienda("Engineering");
+		s.setPiva("123");
+		
+		em.persist(s);
+		
+		em.getTransaction().commit();		
+	}
+	
+	
+	@Test
+	public void test4() {
+		
+		Query query1 = em.createQuery("SELECT u FROM User u WHERE u.email=:email");
+		query1.setParameter("email", "giancarlo.compagno@eng.it");
+		Query query2 = em.createQuery("SELECT s FROM Supplier s");
+		List<User> users = query1.getResultList();
+		for(User user : users) {
+			System.out.println(user.getNome());
+		}
+		
+		users = query2.getResultList();
+		
+		for(User user : users) {
+			System.out.println(user.getNome());
+		}
+		 
+	}
 	
 }
