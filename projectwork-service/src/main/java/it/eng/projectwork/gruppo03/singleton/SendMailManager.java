fe89bf5c37c3477ea279ea8b1fedc17e82929e2f
Ludo <Ludo@Ludo-PC.homenet.telecomitalia.it>
2018-02-22 22:44:16 +0100

    Risolve un problema in sendNewsLetter

--- projectwork-service/src/main/java/it/eng/projectwork/gruppo03/singleton/SendMailManager.java
+++ projectwork-service/src/main/java/it/eng/projectwork/gruppo03/singleton/SendMailManager.java
@@ -5,6 +5,7 @@ import javax.ejb.Schedule;
 import javax.ejb.Singleton;
 import javax.ejb.Startup;
 
+import it.eng.projectwork.gruppo03.mail.Message;
 import it.eng.projectwork.gruppo03.mail.SendMail;
 
 @Singleton
@@ -14,8 +15,12 @@ public class SendMailManager {
 	@EJB
 	SendMail sendMail;
 	
+	@EJB
+	Message message;
+	
 	@Schedule(minute="0/1", persistent=false)
 	public void sendNewsLetter() {
+		sendMail.sendMailAllUser(message);
 		System.out.println("Incoming News");
 	}
 	
